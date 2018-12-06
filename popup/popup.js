125ff1ee242c9ba601ab465f42b6f73647fe06bf
Ayman Salah <ayman171salah@gmail.com>
2018-12-06 04:07:06 +0200

    Added Comments to popup js file

--- popup/popup.js
+++ popup/popup.js
@@ -1,36 +1,13 @@
 (function () {
     'use strict' ;
 
-    // var backgroundpage = chrome.extension.getBackgroundPage();
-    
+
     document.addEventListener("DOMContentLoaded", function(event) {
-        //place all your code here
+        //i used the above function because in first load , DOM elements are empy
         chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
             var url = tabs[0].url;
+            // the two above lines to get the url of current page
             document.getElementById("theURL").innerText = url  ;
         });
     });
 }());
-
-
-
-
-//
-//
-//
-// (function () {
-//     'use strict' ;
-//
-//     var backgroundpage = chrome.extension.getBackgroundPage();
-//
-//     document.addEventListener("DOMContentLoaded", function(event) {
-//         //place all your code here
-//         document.querySelector("button").addEventListener('click' , function () {
-//             backgroundpage.handleButtonClick();
-//
-//         });
-//
-//     });
-//
-// }());
-//
