72960b684aab298dcef3f7dbe66b5a053477f018
Ayman Salah <ayman171salah@gmail.com>
2018-12-06 04:05:27 +0200

    The extension Shows the URL now

--- popup/popup.js
+++ popup/popup.js
@@ -1,35 +1,36 @@
 (function () {
     'use strict' ;
 
-    var backgroundpage = chrome.extension.getBackgroundPage();
-
-
+    // var backgroundpage = chrome.extension.getBackgroundPage();
+    
     document.addEventListener("DOMContentLoaded", function(event) {
         //place all your code here
-        document.querySelector("button").addEventListener('click' , function () {
-            backgroundpage.handleButtonClick();
+        chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
+            var url = tabs[0].url;
+            document.getElementById("theURL").innerText = url  ;
         });
-
     });
-
-    }());
-
+}());
 
 
 
 
-
-
-
+//
+//
+//
 // (function () {
 //     'use strict' ;
 //
 //     var backgroundpage = chrome.extension.getBackgroundPage();
 //
-//     document.querySelector("button").addEventListener('click' , function () {
-//         backgroundpage.handleButtonClick();
+//     document.addEventListener("DOMContentLoaded", function(event) {
+//         //place all your code here
+//         document.querySelector("button").addEventListener('click' , function () {
+//             backgroundpage.handleButtonClick();
+//
+//         });
+//
 //     });
 //
-//
 // }());
-
+//
