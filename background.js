3d4be318d66401b3753726a84538e81b91205ebb
Ayman Salah <ayman171salah@gmail.com>
2018-12-06 03:16:33 +0200

    Added action To button

---
+++ background.js
@@ -0,0 +1,3 @@
+function handleButtonClick() {
+    alert("Hello , We are Working Now !") ;
+}
---
+++ popup/popup.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict' ;
+
+    var backgroundpage = chrome.extension.getBackgroundPage();
+
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+        //place all your code here
+        document.querySelector("button").addEventListener('click' , function () {
+            backgroundpage.handleButtonClick();
+        });
+        
+    });
+
+    }());
