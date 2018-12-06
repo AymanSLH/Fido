74319622e8a1309a4777a9569a71bb720532a0c9
Ayman Salah <ayman171salah@gmail.com>
2018-12-06 23:15:19 +0200

    Added the context menus

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
-function handleButtonClick() {
-
+function getword(info,tab) {
+  chrome.tabs.create({
+    url: "http://www.google.com/search"
+  });
 }
+
+chrome.contextMenus.create({
+  title: "Block This Site",
+  contexts:["all"],
+  onclick: getword
+});
+
+chrome.contextMenus.create({
+  title: "Only Hide This Page",
+  contexts:["all"],
+  onclick: getword
+});
